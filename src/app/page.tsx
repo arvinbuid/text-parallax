c00ddec19bae30035d6713888f9266206e54ab2d
Arvin Buid <arvinbuid01@gmail.com>
2025-07-13 13:54:27 +0800

    Build Home component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,62 @@
+'use client';
+
+import pic1 from '../../public/images/1.jpg';
+import pic2 from '../../public/images/2.jpg';
+import pic3 from '../../public/images/3.jpg';
+import Lenis from 'lenis';
+import Image, { StaticImageData } from 'next/image';
+
+import { useEffect } from 'react';
+
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, [])
+
   return (
-    <>
-      <h1 className="text-3xl font-semibold ml-6 mt-3">Home</h1>
-    </>
+    <main className='overflow-hidden'>
+      <div className="h-[100vh]" />
+      <Slide src={pic1} left={"-40%"} />
+      <Slide src={pic2} left={"-25%"} />
+      <Slide src={pic3} left={"-75%"} />
+      <div className="h-[100vh]" />
+    </main>
   );
+}
+
+interface SlideProps {
+  src: string | StaticImageData;
+  left: string;
+}
+
+const Slide = ({ src, left }: SlideProps) => {
+  return (
+    <div style={{ left }} className="relative flex whitespace-nowrap">
+      <Phrase src={src} />
+      <Phrase src={src} />
+      <Phrase src={src} />
+    </div>
+  )
+}
+
+interface PhraseProps {
+  src: string | StaticImageData;
+}
+
+const Phrase = ({ src }: PhraseProps) => {
+  return (
+    <div className="px-5 flex gap-5 items-center">
+      <p className="text-[7.5vw]">Web Developer</p>
+      <span className="relative h-[7.5vw] aspect-[4/2] rounded-full overflow-hidden">
+        <Image src={src} style={{ objectFit: 'cover' }} alt='images' fill sizes='(max-width: 768px) 100vw, 33vw' />
+      </span>
+    </div>
+  )
 }
