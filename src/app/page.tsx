713932f72fee28b98d8c320a34645392cb0207e5
Arvin Buid <arvinbuid01@gmail.com>
2025-07-13 09:27:17 +0800

    style: add custom font.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <><h1 className="text-3xl font-semibold ml-6 mt-3">Home</h1></>
+    <>
+      <h1 className="text-3xl font-semibold ml-6 mt-3">Home</h1>
+    </>
   );
 }
